ad8d07488240e4557ecbc62ae818416eee497e4e
mintRaven <debjeetbanerjee48@gmail.com>
2025-03-02 20:00:04 +0530

    [CHORE]: kagezoShell command handler header

---
+++ shell/include/commands.hpp
@@ -0,0 +1,10 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include <string>
+#include <vector>
+
+void executeScript(const std::string &script,
+                   const std::vector<std::string> &args);
+
+#endif
