1020eaeb7b54d9f0e3283bf29cebc6204e6c7f03
mintRaven <debjeetbanerjee48@gmail.com>
2025-03-03 12:25:18 +0530

    [DEBUG]: verbose log output

---
+++ tracker/logger.go
@@ -0,0 +1,20 @@
+package tracker
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func logChange(message string) {
+
+	f, err := os.OpenFile(localpath+"output.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening log file:", err)
+		return
+	}
+	defer f.Close()
+
+	logMessage := fmt.Sprintf("[%s] %s\n", time.Now().Format("2006-01-02 15:04:05"), message)
+	f.WriteString(logMessage)
+}
