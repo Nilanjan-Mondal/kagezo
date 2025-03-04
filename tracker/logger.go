27f31bbfa298541561c9e1eabdd149b804c4bc98
mintRaven <debjeetbanerjee48@gmail.com>
2025-03-04 17:05:11 +0530

    [CHORE]: create a worker logger method

--- tracker/logger.go
+++ tracker/logger.go
@@ -18,3 +18,15 @@ func logChange(message string) {
 	logMessage := fmt.Sprintf("[%s] %s\n", time.Now().Format("2006-01-02 15:04:05"), message)
 	f.WriteString(logMessage)
 }
+func logWorker(message string) {
+
+	f, err := os.OpenFile(localpath+"worker_log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening log file:", err)
+		return
+	}
+	defer f.Close()
+
+	logMessage := fmt.Sprintf("[%s] %s\n", time.Now().Format("2006-01-02 15:04:05"), message)
+	f.WriteString(logMessage)
+}
