6864ff65b373af4b808a3ea1fc3354d55bc5c391
mintRaven <debjeetbanerjee48@gmail.com>
2025-03-07 13:52:21 +0530

    [REFACTOR]: make the code macOS compatible by implementing printf

--- shell/bin/list.sh
+++ shell/bin/list.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Color definitions
 GREEN="\e[32m"
 YELLOW="\e[33m"
 RED="\e[31m"
@@ -9,8 +8,13 @@ BOLD="\e[1m"
 RESET="\e[0m"
 
 services=("track" "worker" "server-unit")
-
-echo -e "${BOLD}${CYAN}∘── Systemd Daemon Status ──∘${RESET}"
+printc() { printf "%b%s%b\n" "$1" "$2" "$RESET"; }
+printc "$CYAN" "                                                   "
+printc "$CYAN" "                                                   "
+printc "$CYAN" "╔═════════════════════════════════════════════════╗"
+printc "$CYAN" "║          ∘── Systemd Daemon Status ──∘          ║"
+printc "$CYAN" "╠═════════════════════════════════════════════════╣"
+printc "$CYAN" "║                                                 ║ "
 for service in "${services[@]}"; do
   status=$(systemctl is-active "$service" 2>/dev/null)
   loaded=$(systemctl is-enabled "$service" 2>/dev/null)
@@ -18,12 +22,16 @@ for service in "${services[@]}"; do
   if [[ $status == "active" ]]; then
     uptime=$(systemctl show -p ActiveEnterTimestamp "$service" | cut -d'=' -f2)
     pid=$(systemctl show -p MainPID "$service" | cut -d'=' -f2)
-    echo -e "  ├─${GREEN}◉${RESET} ${BOLD}$service${RESET} → ${GREEN}Running${RESET}"
-    echo -e "  │   ├─ ${CYAN}Uptime:${RESET} $uptime"
-    echo -e "  │   └─ ${CYAN}PID:${RESET} $pid"
+    printc "    ├─${GREEN}◉${RESET} ${BOLD}$service${RESET} → ${GREEN}" "Running"
+    printc "    │   ├─ ${CYAN}Uptime:${RESET} " "$uptime"
+    printc "    │   └─ ${CYAN}PID:${RESET} " "$pid"
   elif [[ $loaded == "enabled" ]]; then
-    echo -e "  ├─${YELLOW}○${RESET} ${BOLD}$service${RESET} → ${YELLOW}Loaded but Not Running${RESET}"
+    printc "    ├─${YELLOW}○${RESET} ${BOLD}$service${RESET} → ${YELLOW}" "Loaded but Not Running"
   else
-    echo -e "  ├─${RED}⊙${RESET} ${BOLD}$service${RESET} → ${RED}Not Loaded${RESET}"
+    printc "    ├─${RED}⊙${RESET} ${BOLD}$service${RESET} → ${RED}" "Not Loaded"
   fi
 done
+printc "$CYAN" "║                                                 ║ "
+printc "$CYAN" "╚═════════════════════════════════════════════════╝"
+printc "$CYAN" "                                                   "
+printc "$CYAN" "                                                   "
