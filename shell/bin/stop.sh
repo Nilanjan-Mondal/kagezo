9dc7d50b78972314d984729281296f13c899edfb
mintRaven <debjeetbanerjee48@gmail.com>
2025-03-09 08:18:47 +0530

    [REFACTOR]: update help menu

--- shell/bin/stop.sh
+++ shell/bin/stop.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Utility function for kagezoShell (Linux only)
 # Future support for macOS will be added.
+GREEN="\033[32m"
+YELLOW="\033[33m"
+RESET="\033[0m"
 
 [[ "$(uname -s)" != "Linux" ]] && echo "Invalid platform detected" && exit 1
+
+printc() { printf "%b%s%b\n" "$1" "$2" "$RESET"; }
 
 declare -A services=(
   ["tracker"]="track"
@@ -27,7 +32,15 @@ if [[ "$1" == "all" ]]; then
 elif [[ -n "${services[$1]}" ]]; then
   stop_service "$1"
 else
-  echo "Usage: stop [tracker|worker|server|all]"
+  printc ""
+  printc ""
+  printc "$GREEN" "     USAGE:  "
+  printc "$GREEN" "     ◉  Check Daemon Status                         "
+  printc "$YELLOW" "        └─○ list daemon                            "
+  printc "$YELLOW" "            → Show status of all daemons              "
+  printc ""
+  printc ""
+
   exit 0
 fi
-./bin/list.sh
+./bin/list.sh daemon
